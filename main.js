2ccfdb97b851e0fcfedc09bfc7066c090fac76dc
Denisdhl007 <denis.henin@icloud.com>
2023-01-19 09:46:47 +0100

    1

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // // // > https://developer.mozilla.org/fr/docs/Web/API/Node/removeChild
+
 // // > Avec la methode removeChild()
 // // // ## 1. 
 // // // - Utilise le second bouton pour supprimer le premier enfant
@@ -7,10 +8,8 @@
 // // // ## 3 .
 // // // - Utilise le dernier bouton pour supprimer un enfant au hasard
 
-
 // // // > https://developer.mozilla.org/fr/docs/Web/API/Node/replaceChild
 // // // > Avec la methode replaceChild()
-
 
 let button = document.querySelectorAll("input");
 
@@ -29,18 +28,12 @@ button[2].addEventListener('click',()=>{
     li[rand].remove();
 })
 
-
-
-
-
 // // // ## 4 . Dans la partie Remplacement de ton HTML
 // // // - Créer un élément li dans ton fichier JS
 // // // - Remplace le deuxième li par celui ci quand on clique sur le button exercice 4
 
-
-
 let new_li = document.createElement("li");
-new_li.innerText = "nouveau li !";
+new_li.innerText = "nouveau li";
 
 let les_li = document.querySelector("body").children[1].querySelectorAll("li");
 console.log(les_li);
@@ -50,16 +43,8 @@ button[3].addEventListener('click',()=>{
     les_li[1].remove();
 })
 
-
-
-
-
 // // // ## 5. 
 // // // - Créer un programme qui permet avec l'input de créer un li et de remplacer le "li" 3 par celui ci en appuyant sur le bouton exercice 5
-
-
-
-
 
 let bis_li = document.createElement("li");
 
